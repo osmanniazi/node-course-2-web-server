88ad335e6634ae74729e0762dcb2341c0eb06027
unknown <satmap@ISB-GSD-OSMAN.afiniti.com>
2019-01-29 15:51:25 +0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname+'/views/partials');
+app.set('view engine', 'hbs');
+
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+
+    var log = `${now}: ${req.method} ${req.url}`;
+
+    console.log(log);
+    fs.appendFile('server.log', log+'\n', (err)=>{
+        if (err){
+            console.log('Unable to append to sever log');
+        }
+    });
+
+    next();
+});
+
+/*
+app.use((req, res, next) => {
+    res.render('maintainence.hbs', {
+        pageTitle: 'Home Page',
+        // currentYear: new Date().getFullYear(),
+        welcomeMessage: 'Welcome to the Matrix'
+    });
+});
+*/
+
+app.use(express.static(__dirname+'/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+
+app.get('/', (req, res) => {
+    // res.send('<h1>Hello Express</h1>');
+    // res.send({
+    //     name: 'Osman',
+    //     city: 'Islamabad'
+    // });
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        // currentYear: new Date().getFullYear(),
+        welcomeMessage: 'Welcome to the Matrix'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        // currentYear: new Date().getFullYear()
+    });
+});
+
+app.get('/bad', (req, res) => {
+    // res.send('<h1>Hello Express</h1>');
+    res.send({
+        errorMessage: 'Tutts too!'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('server is up on port 3000');
+});
